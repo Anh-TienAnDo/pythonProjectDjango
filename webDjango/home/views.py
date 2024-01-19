50f3c9a4a53de4a30e2c8bc1af5887d79f70a52c
batchanhdao <92378282+batchanhdao@users.noreply.github.com>
2024-01-19 11:03:45 +0700

    19/1 time3

--- webDjango/home/views.py
+++ webDjango/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def Home(req):
     return render(req, 'home.html')
+def HomeUser(req):
+    return render(req, 'user/home.html')
--- webDjango/webDjango/urls.py
+++ webDjango/webDjango/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home import views
+from django.urls import include, path
+from home import views, urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.Home),
     path('', views.Home),
+    path('user/', include('home.urls'))
 ]
