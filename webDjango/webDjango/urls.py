8e7032450252b88f5f126334399c39c96cf8664a
batchanhdao <lea81807@gmail.com>
2024-01-22 21:02:31 +0700

    22/1 time1
    get/set images from DB

--- webDjango/webDjango/urls.py
+++ webDjango/webDjango/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 # from home import views, urls
 
 urlpatterns = [
@@ -25,3 +27,4 @@ urlpatterns = [
     # path('user/', include('home.urls'))
     path('', include('app.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
